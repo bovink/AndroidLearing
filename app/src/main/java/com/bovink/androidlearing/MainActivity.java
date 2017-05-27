c30e6fb18d8eb852e262dc5ee185258ce0da6a64
bovink <bovink@sina.com>
2017-05-27 22:28:46 +0800

    android 存储相关知识

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,7 +1,12 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        File path;
+        // app专属文件
+        // app外部存储
+        path = getExternalFilesDir(null);
+        fun(path,"getExternalFilesDir");
+        // app内部存储
+        path = getFilesDir();
+        fun(path,"getFilesDir");
+        // app外部文件
+        path = Environment.getExternalStorageDirectory();
+        path = new File(path, "File");
+        if (!path.exists()) {
+            path.mkdir();
+        }
+        fun(path, "getExternalStorageDirectory");
+
     }
+
+    private void fun(File path, String name) {
+        File file = new File(path, name + ".txt");
+
+        LogUtils.e("" + path);
+        write(file);
+    }
+
+    private void write(File file) {
+        try {
+            FileOutputStream stream = new FileOutputStream(file);
+            stream.write(file.getName().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
