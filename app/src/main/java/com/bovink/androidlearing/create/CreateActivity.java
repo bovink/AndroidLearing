1fe4f5e928b5056f7a1bfe4c3b7cbb9122d03417
bovink <bovink@sina.com>
2017-06-07 22:32:01 +0800

    创建Observable的静态工厂方法sample

--- app/src/main/java/com/bovink/androidlearing/create/CreateActivity.java
+++ app/src/main/java/com/bovink/androidlearing/create/CreateActivity.java
@@ -7,8 +7,14 @@ import android.widget.Button;
 
 import com.bovink.androidlearing.R;
 
+import java.util.concurrent.TimeUnit;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.TestScheduler;
 
 /**
  * com.bovink.androidlearing.create
@@ -30,7 +36,19 @@ public class CreateActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_create)
     void clickBtn(Button button) {
-       button.setText("clicked button");
 
+        TestScheduler testScheduler = new TestScheduler();
+
+        Observable<Long> observable = CreateUtils.timer(testScheduler);
+
+        observable.subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(@NonNull Long aLong) throws Exception {
+                System.out.println("aLong = " + aLong);
+
+            }
+        });
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MINUTES);
     }
 }
