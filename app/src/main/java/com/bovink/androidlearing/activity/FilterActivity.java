456da7a2e2a10f1584682391b66949291dd00e17
bovink@sina.com <261512335wen>
2017-06-16 13:38:38 +0800

    新建FilterActivity

---
+++ app/src/main/java/com/bovink/androidlearing/activity/FilterActivity.java
@@ -0,0 +1,22 @@
+package com.bovink.androidlearing.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bovink.androidlearing.R;
+
+/**
+ * Created by Retina975 on 17/6/16.
+ */
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+
+
+}
