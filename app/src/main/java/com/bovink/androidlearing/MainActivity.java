47d85d8fcc1a8365a543dcb536412276b059a95e
bovink@sina.com <261512335wen>
2017-06-28 10:52:50 +0800

    使用第三方框架加载album

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,7 +1,14 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.finalteam.rxgalleryfinal.RxGalleryFinalApi;
+import cn.finalteam.rxgalleryfinal.rxbus.RxBusResultSubscriber;
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageMultipleResultEvent;
+import cn.finalteam.rxgalleryfinal.utils.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
+    @OnClick(R.id.btn_custom_album)
+    void album() {
+
+        RxGalleryFinalApi.getInstance(this).setImageMultipleResultEvent(new RxBusResultSubscriber<ImageMultipleResultEvent>() {
+            @Override
+            protected void onEvent(ImageMultipleResultEvent imageMultipleResultEvent) throws Exception {
+                Logger.i("多选图片的回调");
+            }
+        }).open();
+    }
+
 }
