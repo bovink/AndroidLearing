ff8a4df5680a1fe0434afd7fd0023b9864593f7d
bovink <bovink@sina.com>
2017-07-04 23:17:40 +0800

    给recyclerview添加scrollbar

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,45 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.fl_target)
+    FrameLayout targetFl;
 
+    boolean targetShowed = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btn_click)
+    void clickBtn() {
+        if (targetShowed) {
+
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(targetFl, "Y", 0, 1000);
+            objectAnimator.setInterpolator(new LinearOutSlowInInterpolator());
+            objectAnimator.setDuration(800);
+            objectAnimator.start();
+        }else {
+
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(targetFl, "Y", 1000, 0);
+            objectAnimator.setInterpolator(new LinearOutSlowInInterpolator());
+            objectAnimator.setDuration(800);
+            objectAnimator.start();
+        }
+        targetShowed = !targetShowed;
+
     }
 }
