fcc17f9aa1f2a0dd424f78fec538c1c5c5e63bba
bovink@sina.com <261512335wen>
2017-07-07 15:04:10 +0800

    使用viewpager

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,45 @@
 package com.bovink.androidlearing;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.vp_image)
+    ViewPager imageViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter(getSupportFragmentManager());
+        imageViewPager.setAdapter(myViewPagerAdapter);
+    }
+
+    private class MyViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MainFragment mainFragment = MainFragment.getInstance(position);
+            return mainFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 6;
+        }
     }
 }
--- app/src/main/java/com/bovink/androidlearing/MainFragment.java
+++ app/src/main/java/com/bovink/androidlearing/MainFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author Retina975
@@ -13,11 +17,24 @@ import android.view.ViewGroup;
  */
 
 public class MainFragment extends Fragment {
+    private int index;
+
+    @BindView(R.id.tv_index)
+    TextView indexTextView;
+
+    public static MainFragment getInstance(int index) {
+        MainFragment mainFragment = new MainFragment();
+        mainFragment.index = index;
+        return mainFragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, root);
+
+        indexTextView.setText("第" + index + "个");
         return root;
     }
 }
