bdd57f8489e9ae77018eac42cd006a7380f80d40
bovink <bovink@sina.com>
2017-07-14 00:14:28 +0800

    测试Activity之间RxBus传递事件

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -3,11 +3,25 @@ package com.bovink.androidlearing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_click)
+    void onClick() {
+        try {
+            HelloClient client = new HelloClient();
+            client.connect("59.175.213.77", 30161);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
