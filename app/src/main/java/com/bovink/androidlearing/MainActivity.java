c05da55717de11ad7dd9a18302fd4cdf187f9165
bovink@sina.com <261512335wen>
2017-07-14 09:41:41 +0800

    使用viewpager

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,22 +1,53 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    User userA;
+    User userB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        List<String> ids = new ArrayList<>();
+        ids.add("jhoa_single");
+
+        userA = new User();
+        userA.setAppId("dudao");
+        userA.setGroupIds(ids);
+        userA.setUserName("hetianjiao");
+        userA.setUserUid("117");
+
+
+        userB = new User();
+        userB.setAppId("dudao");
+        userB.setGroupIds(ids);
+        userB.setUserName("fox");
+        userB.setUserUid("126");
     }
 
-    @OnClick(R.id.btn_click)
-    void onClick() {
+    @OnClick(R.id.btn_login_a)
+    void loginA() {
+        try {
+            HelloClient client = new HelloClient();
+            client.connect("59.175.213.77", 30161);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick(R.id.btn_login_b)
+    void loginB() {
         try {
             HelloClient client = new HelloClient();
             client.connect("59.175.213.77", 30161);
---
+++ app/src/main/java/com/bovink/androidlearing/MessageSender.java
@@ -0,0 +1,45 @@
+package com.bovink.androidlearing;
+
+/**
+ * @author Retina975
+ * @since 2017/07/14
+ */
+
+public class MessageSender {
+    private String messNote;
+    private String messType;
+    private String groupId;
+    private String receiverUid;
+
+    public String getMessNote() {
+        return messNote;
+    }
+
+    public void setMessNote(String messNote) {
+        this.messNote = messNote;
+    }
+
+    public String getMessType() {
+        return messType;
+    }
+
+    public void setMessType(String messType) {
+        this.messType = messType;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getReceiverUid() {
+        return receiverUid;
+    }
+
+    public void setReceiverUid(String receiverUid) {
+        this.receiverUid = receiverUid;
+    }
+}
