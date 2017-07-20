968a17b8a45436cf07b98c90bec26710cdf7ec61
bovink@sina.com <261512335wen>
2017-07-20 11:03:54 +0800

    使用GreenDAO自动生成代码

---
+++ app/src/main/java/com/bovink/androidlearing/User.java
@@ -0,0 +1,47 @@
+package com.bovink.androidlearing;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * @author Retina975
+ * @since 2017/07/19
+ */
+
+@Entity
+public class User  {
+    @Id(autoincrement = true)
+    private Long id;
+    @Property(nameInDb = "USERNAME")
+    private String name;
+    private int age;
+    @Generated(hash = 1309193360)
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    @Generated(hash = 586692638)
+    public User() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
