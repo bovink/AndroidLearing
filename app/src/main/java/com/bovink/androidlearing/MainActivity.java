49e28670bbea932d030b94d160dba13edfa5c0fd
bovink@sina.com <261512335wen>
2017-07-21 09:29:40 +0800

    EditText

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -2,12 +2,24 @@ package com.bovink.androidlearing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_text)
+    EditText et_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        float scale = getResources().getDisplayMetrics().density;
+        System.out.println("scale = " + scale);
+
+
     }
 }
