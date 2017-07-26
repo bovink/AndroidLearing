c000a39e7cf7e489fd8c8e97c28730cb859ac172
bovink@sina.com <261512335wen>
2017-07-26 11:19:38 +0800

    录制音频并播放

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,125 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    private String audioFileName;
+
+    private MediaRecorder recorder;
+    private MediaPlayer player;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        System.out.println("MainActivity.onRequestPermissionsResult");
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        audioFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AndroidLearning";
+        File file = new File(audioFileName);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        audioFileName += "/test.3gp";
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (recorder != null) {
+            recorder.release();
+            recorder = null;
+        }
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    @OnClick(R.id.btn_start_record)
+    void startRecord() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(audioFileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+
+    }
+
+    @OnClick(R.id.btn_stop_record)
+    void stopRecord() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    @OnClick(R.id.btn_start_play)
+    void startPlay() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+
+        player = new MediaPlayer();
+
+        try {
+            player.setDataSource(audioFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        player.start();
+
+    }
+
+    @OnClick(R.id.btn_stop_play)
+    void stopPlay() {
+        player.release();
+        player = null;
     }
 }
