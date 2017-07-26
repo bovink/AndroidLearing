ae510de7417e261b20aa9fb1aa3e46c2cd1cb73b
bovink@sina.com <261512335wen>
2017-07-26 12:25:51 +0800

    播放视频

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,29 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.videoview)
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        String file = Environment.getExternalStorageDirectory() + "/Video/video.mp4";
+        videoView.setVideoPath(file);
+
+        videoView.start();
+        videoView.setMediaController(new MediaController(this));
+
     }
 }
