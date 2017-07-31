91cc14626e907bf3d256683b8f933885cbedf2c6
bovink@sina.com <261512335wen>
2017-07-31 17:09:21 +0800

    画线

--- app/src/main/java/com/bovink/androidlearing/WeiXinImageView.java
+++ app/src/main/java/com/bovink/androidlearing/WeiXinImageView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
@@ -33,8 +34,14 @@ public class WeiXinImageView extends AppCompatImageView {
 
         init(canvas);
         super.draw(canvas);
+        drawStroke(canvas);
     }
 
+    /**
+     * 切图
+     *
+     * @param canvas
+     */
     private void init(Canvas canvas) {
         int space = 50;
 
@@ -56,6 +63,43 @@ public class WeiXinImageView extends AppCompatImageView {
         path.lineTo(0, space * 3 / 2);
         path.lineTo(space / 2, space * 2);
         canvas.clipPath(path);
+
+    }
+
+    /**
+     * 画线
+     *
+     * @param canvas
+     */
+    private void drawStroke(Canvas canvas) {
+        int space = 50;
+
+        Paint penPaint = new Paint();
+        penPaint.setColor(Color.GREEN);
+        penPaint.setAntiAlias(true);
+        penPaint.setStyle(Paint.Style.STROKE);
+
+        Path path = new Path();
+        path.moveTo(space / 2, 0);
+        path.lineTo(space / 2, space);
+        path.lineTo(0, space * 3 / 2);
+        path.lineTo(space / 2, space * 2);
+        path.lineTo(space / 2, getHeight());
+        path.lineTo(getWidth(), getHeight());
+        path.lineTo(getWidth(), 0);
+        path.lineTo(space / 2, 0);
+
+
+        RectF rectF = new RectF(space / 2, 0, space * 3 / 2, space);
+        path.addArc(rectF, 180, 90);
+        rectF = new RectF(space / 2, getHeight() - space, space * 3 / 2, getHeight());
+        path.addArc(rectF, 90, 90);
+        rectF = new RectF(getWidth() - space, getHeight() - space, getWidth(), getHeight());
+        path.addArc(rectF, 0, 90);
+        rectF = new RectF(getWidth() - space, 0, getWidth(), space);
+        path.addArc(rectF, 270, 90);
+
+        canvas.drawPath(path, penPaint);
 
     }
 }
