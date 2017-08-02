12d91d219d39ec33e389bc408a21a5d3a1d12a31
bovink <bovink@sina.com>
2017-08-02 21:02:53 +0800

    使用Glide加载图片

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,27 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.iv_image)
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Glide.with(this)
+                .load("http://ww4.sinaimg.cn/mw690/005ZZ4X1gw1f78nyq4ygzj30uc0j60vc.jpg")
+                .asBitmap()
+                .into(imageView);
     }
 }
