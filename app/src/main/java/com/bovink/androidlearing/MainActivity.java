8ee08b6989faef07236aa3c94fec8801822094d2
bovink@sina.com <261512335wen>
2017-08-08 11:07:04 +0800

    创建VideoActivity

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_video)
+    void showVideo() {
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/bovink/androidlearing/VideoActivity.java
@@ -0,0 +1,19 @@
+package com.bovink.androidlearing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Retina975
+ * @since 2017/08/08
+ */
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
