8fc65fd577f42a70dc7bf6e39f0a716aa2685afb
bovink@sina.com <261512335wen>
2017-08-17 14:15:01 +0800

    打开摄像头

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -6,17 +6,18 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.TextureView;
 
 import java.io.IOException;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.view_surface)
-    SurfaceView surfaceView;
+    @BindView(R.id.view_texture)
+    TextureView textureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +28,43 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 2);
-        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+
+    @OnClick(R.id.btn_record)
+    void record() {
+
+
+        openCamera();
+    }
+
+    private void openCamera() {
+
         Camera camera = Camera.open();
 
-        camera.setDisplayOrientation(180);
-
-        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        camera.setDisplayOrientation(90);
 
         try {
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
+            camera.setPreviewTexture(textureView.getSurfaceTexture());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Camera.Parameters parameters = camera.getParameters();
+
+        List<String> focusList = parameters.getSupportedFocusModes();
+        if (focusList.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        } else if (focusList.contains(Camera.Parameters.FOCUS_MODE_FIXED)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_FIXED);
+        } else if (focusList.contains(Camera.Parameters.FOCUS_MODE_INFINITY)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+        } else {
+            parameters.setFocusMode(focusList.get(0));
+        }
+
+        camera.setParameters(parameters);
+        camera.startPreview();
     }
+
 }
