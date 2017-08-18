e1e3814d61866cb16ce298009d170cb3da6ac6ce
bovink@sina.com <261512335wen>
2017-08-18 11:07:39 +0800

    设置预览Size和照片Size

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -4,11 +4,17 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.TextureView;
+import android.view.WindowManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 import butterknife.BindView;
@@ -19,9 +25,13 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.view_texture)
     TextureView textureView;
 
+    Camera camera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
@@ -40,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void openCamera() {
 
-        Camera camera = Camera.open();
+        camera = Camera.open();
 
         camera.setDisplayOrientation(90);
 
@@ -51,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Camera.Parameters parameters = camera.getParameters();
+        parameters.setPreviewSize(1920, 1080);
+        parameters.setPictureSize(1920, 1080);
+
 
         List<String> focusList = parameters.getSupportedFocusModes();
         if (focusList.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
@@ -63,8 +76,95 @@ public class MainActivity extends AppCompatActivity {
             parameters.setFocusMode(focusList.get(0));
         }
 
+//        String originAspectRatio = getAspectRatio(textureView.getHeight(), textureView.getWidth());
+//        System.out.println("originAspectRatio = " + originAspectRatio);
+//        System.out.println("textureView.getWidth() = " + textureView.getWidth());
+//        System.out.println("textureView.getHeight() = " + textureView.getHeight());
+//
+//        List<Camera.Size> previewSizes = new ArrayList<>();
+//        List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
+//        for (Camera.Size size : sizes) {
+//            String previewAspectRatio = getAspectRatio(size.width, size.height);
+//            if (previewAspectRatio.equals(originAspectRatio)) {
+//                System.out.println("previewAspectRatio = " + previewAspectRatio);
+//                System.out.println("size.width = " + size.width);
+//                System.out.println("size.height = " + size.height);
+//            }
+//        }
+
         camera.setParameters(parameters);
         camera.startPreview();
     }
 
+    @OnClick(R.id.btn_capture)
+    void capture() {
+        System.out.println("MainActivity.capture");
+
+        takePicture();
+//        camera.cancelAutoFocus();
+//        camera.autoFocus(new Camera.AutoFocusCallback() {
+//            @Override
+//            public void onAutoFocus(boolean success, Camera camera) {
+//            }
+//        });
+    }
+
+    private void takePicture() {
+
+        camera.takePicture(null, null, null, new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(final byte[] data, Camera camera) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/" + "MainActivity.jpg");
+                        System.out.println("file.getAbsolutePath() = " + file.getAbsolutePath());
+                        OutputStream os = null;
+                        try {
+                            os = new FileOutputStream(file);
+                            os.write(data);
+                            os.close();
+                        } catch (IOException e) {
+                            Log.w("MainActivity", "Cannot write to " + file, e);
+                        } finally {
+                            if (os != null) {
+                                try {
+                                    os.close();
+                                } catch (IOException e) {
+                                    // Ignore
+                                }
+                            }
+                        }
+                    }
+                }.start();
+            }
+        });
+        camera.startPreview();
+    }
+
+
+    private String getAspectRatio(int width, int height) {
+        int x = width;
+        int y = height;
+        int gcd = gcd(x, y);
+        x /= gcd;
+        y /= gcd;
+        return x + ":" + y;
+    }
+
+    /**
+     * 获取最小公约数
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int c = b;
+            b = a % b;
+            a = c;
+        }
+        return a;
+    }
 }
