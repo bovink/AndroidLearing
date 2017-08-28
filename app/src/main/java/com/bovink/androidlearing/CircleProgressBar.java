2e68892695f1ea7fd3c1b83371d6db92e42ebd73
bovink@sina.com <261512335wen>
2017-08-28 15:15:53 +0800

    使用Handler替代RxJava

--- app/src/main/java/com/bovink/androidlearing/CircleProgressBar.java
+++ app/src/main/java/com/bovink/androidlearing/CircleProgressBar.java
@@ -5,21 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * @author Retina975
@@ -64,7 +56,6 @@ public class CircleProgressBar extends AppCompatImageView {
      * 角度
      */
     float angel = 0;
-    private CompositeDisposable disposable;
 
     public CircleProgressBar(Context context) {
         super(context);
@@ -82,7 +73,6 @@ public class CircleProgressBar extends AppCompatImageView {
     }
 
     private void init() {
-        disposable = new CompositeDisposable();
 
         paint = new Paint();
         paint.setAntiAlias(true);
@@ -116,7 +106,8 @@ public class CircleProgressBar extends AppCompatImageView {
                 switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_CANCEL:
                     case MotionEvent.ACTION_UP:
-                        disposable.clear();
+                        circleHandler.removeMessages(1001);
+                        progressHandler.removeMessages(1001);
                         break;
                 }
                 return false;
@@ -125,42 +116,17 @@ public class CircleProgressBar extends AppCompatImageView {
     }
 
     private void startProgress() {
-
-        disposable.add(Observable.intervalRange(0, 10, 0, 10, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Long>() {
-                    @Override
-                    public void accept(@NonNull Long aLong) throws Exception {
-                        outRadius += 3.5;
-                        innerRadius -= 3.5;
-                        invalidate();
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(@NonNull Throwable throwable) throws Exception {
-                        throwable.printStackTrace();
-                    }
-                }, new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        rectF.set(width / 2 - outRadius - 5, width / 2 - outRadius - 5, width / 2 + outRadius + 5, width / 2 + outRadius + 5);
-                        adjustCircleProgress();
-                    }
-                }));
+        Message msg = new Message();
+        msg.what = 1001;
+        msg.arg1 = 1;
+        circleHandler.sendMessage(msg);
     }
 
     private void adjustCircleProgress() {
-        disposable.add(Observable.intervalRange(1, 1000, 0, 10, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Long>() {
-                    @Override
-                    public void accept(@NonNull Long aLong) throws Exception {
-                        float percent = (float) aLong / 1000;
-                        setProgress(percent);
-                    }
-                }));
+        Message msg = new Message();
+        msg.what = 1001;
+        msg.arg1 = 1;
+        progressHandler.sendMessage(msg);
     }
 
     @Override
@@ -187,4 +153,52 @@ public class CircleProgressBar extends AppCompatImageView {
         invalidate();
 
     }
+
+    private Handler circleHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1001) {
+                // 开始绘制
+                outRadius += 3.5;
+                innerRadius -= 3.5;
+                invalidate();
+
+                Message nextMsg;
+                // 执行到10为止
+                if (msg.arg1 != 10) {
+                    nextMsg = obtainMessage();
+                    nextMsg.what = msg.what;
+                    nextMsg.arg1 = msg.arg1 + 1;
+                    sendMessageDelayed(nextMsg, 10);
+                } else {
+                    // 开始进度
+                    nextMsg = obtainMessage();
+                    nextMsg.what = 1002;
+                    sendMessage(nextMsg);
+                }
+            } else if (msg.what == 1002) {
+                rectF.set(width / 2 - outRadius - 5, width / 2 - outRadius - 5, width / 2 + outRadius + 5, width / 2 + outRadius + 5);
+                adjustCircleProgress();
+            }
+        }
+    };
+
+    private Handler progressHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1001) {
+                // 开始绘制
+                float percent = (float) msg.arg1 / 1000;
+                setProgress(percent);
+
+                // 执行1000次
+                if (msg.arg1 != 1000) {
+                    Message nextMsg = obtainMessage();
+                    nextMsg.what = msg.what;
+                    nextMsg.arg1 = msg.arg1 + 1;
+                    sendMessageDelayed(nextMsg, 10);
+                }
+            }
+        }
+    };
 }
