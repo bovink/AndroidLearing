a92f5a180d9cf774d2d8fcb014de6a0fd93f8305
bovink <bovink@sina.com>
2017-09-24 18:49:47 +0800

    初步使用TabLayout

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,59 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.vp_content)
+    ViewPager contentViewPager;
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> titleList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        fragmentList.add(new FirstFragment());
+        fragmentList.add(new SecondFragment());
+        titleList.add("一");
+        titleList.add("二");
+
+        contentViewPager.setAdapter(new MyFragmentStatePagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class MyFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyFragmentStatePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titleList.get(position);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
     }
 }
---
+++ app/src/main/java/com/bovink/androidlearing/SecondFragment.java
@@ -0,0 +1,25 @@
+package com.bovink.androidlearing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * com.bovink.androidlearing
+ *
+ * @author bovink
+ * @since 2017/9/24
+ */
+
+public class SecondFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_second, container, false);
+        return root;
+    }
+}
