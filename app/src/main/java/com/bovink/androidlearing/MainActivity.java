2e0e37fca29d3f9b42b87ec6e323aa3b33879923
bovink@sina.com <261512335wen>
2017-10-18 11:30:06 +0800

    LineChart的基础使用

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -4,6 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +26,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        initLineChart();
+    }
+
+    private void initLineChart() {
+        exampleLineChart.setTouchEnabled(false);
+        exampleLineChart.getDescription().setEnabled(false);
+
+        Legend legend = exampleLineChart.getLegend();
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+
+        XAxis xAxis = exampleLineChart.getXAxis();
+        xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        YAxis leftYAxis = exampleLineChart.getAxisLeft();
+        leftYAxis.setMaxWidth(10);
+        leftYAxis.setAxisMinimum(0);
+        leftYAxis.setAxisMaximum(55);
+
+        YAxis rightYAxis = exampleLineChart.getAxisRight();
+        rightYAxis.setDrawLabels(false);
+        rightYAxis.setDrawAxisLine(false);
+        rightYAxis.setDrawGridLines(false);
+
+        setData();
+    }
+
+    private void setData() {
+
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            entries.add(new Entry(i, (float) (Math.random() * 50)));
+        }
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "1");
+
+        LineData lineData = new LineData(lineDataSet);
+        lineData.setDrawValues(false);
+
+        exampleLineChart.setData(lineData);
 
     }
 
