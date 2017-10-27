553dee53b51904a725aa4a23186ad40bfbd2f21c
bovink@sina.com <261512335wen>
2017-10-27 09:46:44 +0800

    绘制Legend

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +29,9 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         exampleBarChart.getDescription().setEnabled(false);
-        exampleBarChart.getLegend().setEnabled(false);
 
         XAxis xAxis = exampleBarChart.getXAxis();
-        xAxis.setDrawLabels(false);
+        xAxis.setDrawLabels(true);
         xAxis.setDrawGridLines(false);
         xAxis.setDrawAxisLine(false);
 
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         leftYAxis.setDrawLabels(false);
         leftYAxis.setDrawGridLines(false);
         leftYAxis.setDrawAxisLine(false);
+        leftYAxis.setDrawZeroLine(true);
         leftYAxis.setAxisMaximum(100);
 
         YAxis rightYAxis = exampleBarChart.getAxisRight();
@@ -46,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         rightYAxis.setDrawAxisLine(false);
 
         List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 30f));
+        entries.add(new BarEntry(0f, 100f));
         entries.add(new BarEntry(1f, 80f));
         entries.add(new BarEntry(2f, 60f));
         entries.add(new BarEntry(3f, 50f));
@@ -54,13 +56,29 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new BarEntry(5f, 70f));
         entries.add(new BarEntry(6f, 60f));
 
+        List<BarEntry> entries2 = new ArrayList<>();
+        entries2.add(new BarEntry(0f, -100f));
+
         BarDataSet set = new BarDataSet(entries, "BarDataSet");
 
-        BarData data = new BarData(set);
+        BarDataSet set2 = new BarDataSet(entries2, "aaa");
+
+        List<IBarDataSet> sets = new ArrayList<>();
+        sets.add(set);
+        sets.add(set2);
+
+        BarData data = new BarData(sets);
         data.setBarWidth(0.9f); // set custom bar width
         data.setDrawValues(false);
         exampleBarChart.setData(data);
         exampleBarChart.setFitBars(true); // make the x-axis fit exactly all bars
+
+        Legend legend = exampleBarChart.getLegend();
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        legend.setDirection(Legend.LegendDirection.LEFT_TO_RIGHT);
+        legend.setDrawInside(true);
+        legend.setOrientation(Legend.LegendOrientation.VERTICAL);
     }
 
 }
