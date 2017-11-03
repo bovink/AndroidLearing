ed728703feddb64596fdf0cf12eeaea1c59e8467
bovink@sina.com <261512335wen>
2017-11-03 15:46:00 +0800

    fragment操作

---
+++ app/src/main/java/com/bovink/androidlearing/FirstFragment.java
@@ -0,0 +1,23 @@
+package com.bovink.androidlearing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author fox
+ * @since 2017/11/03
+ */
+
+public class FirstFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_first, container, false);
+        return root;
+    }
+}
--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,52 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirstFragment firstFragment;
+    SecondFragment secondFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        firstFragment = new FirstFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fl_main, firstFragment)
+                .show(firstFragment)
+                .commit();
+
+    }
+
+    @OnClick(R.id.btn_one)
+    void showOne() {
+
+        getSupportFragmentManager().beginTransaction()
+                .show(firstFragment)
+                .commit();
+    }
+
+    @OnClick(R.id.btn_two)
+    void showTwo() {
+        if (secondFragment == null) {
+            secondFragment = new SecondFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fl_main, secondFragment)
+                    .commit();
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .show(secondFragment)
+                .commit();
     }
 }
