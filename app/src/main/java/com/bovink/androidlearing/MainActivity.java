010a936b30ab77b66f7b6d0945803fb1ced63848
bovink@sina.com <261512335wen>
2017-11-15 14:27:08 +0800

    数据绑定

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,20 @@
 package com.bovink.androidlearing;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bovink.androidlearing.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Person person = new Person("lilei", "23");
+        binding.setPerson(person);
+
     }
 }
