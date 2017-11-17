08fbd383995e055d143bf5c4407c35ddce2c9ba9
bovink@sina.com <261512335wen>
2017-11-17 14:23:43 +0800

    解决使用greendao报的找不到依赖的class path警告

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -3,6 +3,13 @@ package com.bovink.androidlearing;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.bovink.androidlearing.gen.DaoMaster;
+import com.bovink.androidlearing.gen.DaoSession;
+import com.bovink.androidlearing.gen.UserDao;
+import com.bovink.androidlearing.orm.User;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "user.db", null);
+        DaoMaster daoMaster = new DaoMaster(helper.getEncryptedWritableDb("123"));
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+
+        User extraUser = new User(null, "jj", "44");
+        userDao.insert(extraUser);
+
+        List<User> userList = userDao.loadAll();
+        for (User user: userList) {
+            System.out.println("user.getId() = " + user.getId());
+            System.out.println("user.getName() = " + user.getName());
+            System.out.println("user.getAge() = " + user.getAge());
+
+        }
 
     }
 }
