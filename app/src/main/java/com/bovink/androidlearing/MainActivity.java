529ba8865082652b16d38297000d67e8d9e9d925
bovink@sina.com <261512335wen>
2017-11-17 13:55:10 +0800

    删除废弃代码

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -3,11 +3,6 @@ package com.bovink.androidlearing;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.bovink.androidlearing.gen.UserDao;
-import com.bovink.androidlearing.orm.User;
-
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,15 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        UserDao userDao = GreenDaoManager.getInstance(getApplicationContext()).getNewSession().getUserDao();
-
-//        User user = new User(null, "jane", "13");
-//        userDao.insert(user);
-        List<User> userList = userDao.loadAll();
-        for (User user: userList) {
-            System.out.println("user.getId() = " + user.getId());
-            System.out.println("user.getName() = " + user.getName());
-            System.out.println("user.getAge() = " + user.getAge());
-        }
     }
 }
