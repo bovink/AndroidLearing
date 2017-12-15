bed04d4c3ba45c15a04d60f6919fb4c76ad9257d
bovink@sina.com <261512335wen>
2017-12-15 14:43:35 +0800

    初始化EventManager与EventListener；

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,13 +1,74 @@
 package com.bovink.androidlearing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
+
+import org.json.JSONObject;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EventManager eventManager;
+
+    private EventListener eventListener = new EventListener() {
+        @Override
+        public void onEvent(String name, String params, byte[] data, int offset, int length) {
+            System.out.println("name = " + name);
+            System.out.println("params = " + params);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        release();
+        super.onDestroy();
+    }
+
+    private void initEventManager() {
+        eventManager = EventManagerFactory.create(this, "asr");
+        eventManager.registerListener(eventListener);
+
+    }
+
+    private void start(Map<String, Object> params) {
+
+        String json = new JSONObject(params).toString();
+        System.out.println(json);
+        eventManager.send(SpeechConstant.ASR_START, json, null, 0, 0);
+    }
+
+    private void stop() {
+        eventManager.send(SpeechConstant.ASR_STOP, "{}", null, 0, 0);
+
+    }
+
+    private void cancel() {
+
+        if (eventManager != null) {
+            eventManager.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+        }
+    }
+
+    public void release() {
+
+        if (eventManager == null) {
+            return;
+        }
+
+        cancel();
+        eventManager.unregisterListener(eventListener);
+        eventManager = null;
     }
 }
