b5dbfa84bb9ef785eed3d7bd4d3f9b5f8de38276
bovink@sina.com <261512335wen>
2017-12-20 11:50:37 +0800

    分离语音识别方法；

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -5,10 +5,6 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.baidu.speech.EventListener;
-import com.baidu.speech.EventManager;
-import com.baidu.speech.EventManagerFactory;
-import com.baidu.speech.asr.SpeechConstant;
 import com.bovink.androidlearing.network.ApiUtils;
 import com.bovink.androidlearing.utils.DeviceUtils;
 
@@ -46,84 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
     private CompositeDisposable compositeDisposable;
 
-    private EventManager eventManager;
-
-    private EventListener eventListener;
-
-    private IRecogListener iRecogListener = new IRecogListener() {
-        @Override
-        public void onAsrReady() {
-
-
-        }
-
-        @Override
-        public void onAsrBegin() {
-
-        }
-
-        @Override
-        public void onAsrEnd() {
-
-        }
-
-        @Override
-        public void onAsrPartialResult(String[] results, RecogResult recogResult) {
-
-            textView.setText(results[0]);
-        }
-
-        @Override
-        public void onAsrFinalResult(String[] results, RecogResult recogResult) {
-
-            textView.setText(results[0]);
-        }
-
-        @Override
-        public void onAsrFinish(RecogResult recogResult) {
-
-        }
-
-        @Override
-        public void onAsrFinishError(int errorCode, int subErrorCode, String errorMessage, String descMessage, RecogResult recogResult) {
-
-        }
-
-        @Override
-        public void onAsrLongFinish() {
-
-        }
-
-        @Override
-        public void onAsrVolume(int volumePercent, int volume) {
-
-        }
-
-        @Override
-        public void onAsrAudio(byte[] data, int offset, int length) {
-
-        }
-
-        @Override
-        public void onAsrExit() {
-
-        }
-
-        @Override
-        public void onAsrOnlineNluResult(String nluResult) {
-
-        }
-
-        @Override
-        public void onOfflineLoaded() {
-
-        }
-
-        @Override
-        public void onOfflineUnLoaded() {
-
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,18 +51,12 @@ public class MainActivity extends AppCompatActivity {
 
         compositeDisposable = new CompositeDisposable();
 
-        initEventManager();
 
         testFileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES) + "/16k.pcm";
         System.out.println("testFileName = " + testFileName);
 //        getAccessToken();
     }
 
-    @Override
-    protected void onDestroy() {
-        release();
-        super.onDestroy();
-    }
 
     @OnClick(R.id.btn_click)
     void onClick() {
@@ -179,42 +91,7 @@ public class MainActivity extends AppCompatActivity {
 //                }));
     }
 
-    private void initEventManager() {
-        eventManager = EventManagerFactory.create(this, "asr");
-        eventListener = new RecogEventAdapter(iRecogListener);
-        eventManager.registerListener(eventListener);
 
-    }
-
-    private void start(Map<String, Object> params) {
-
-        String json = new JSONObject(params).toString();
-        System.out.println(json);
-        eventManager.send(SpeechConstant.ASR_START, json, null, 0, 0);
-    }
-
-    private void stop() {
-        eventManager.send(SpeechConstant.ASR_STOP, "{}", null, 0, 0);
-
-    }
-
-    private void cancel() {
-
-        if (eventManager != null) {
-            eventManager.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
-        }
-    }
-
-    public void release() {
-
-        if (eventManager == null) {
-            return;
-        }
-
-        cancel();
-        eventManager.unregisterListener(eventListener);
-        eventManager = null;
-    }
 
     private void getAccessToken() {
         // AccessToken
--- app/src/main/java/com/bovink/androidlearing/activity/VoiceIdentifyActivity.java
+++ app/src/main/java/com/bovink/androidlearing/activity/VoiceIdentifyActivity.java
@@ -4,6 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
+import com.bovink.androidlearing.IRecogListener;
+import com.bovink.androidlearing.RecogEventAdapter;
+import com.bovink.androidlearing.RecogResult;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
 /**
  * @author fox
  * @since 2017/12/20
@@ -11,8 +23,129 @@ import android.support.v7.app.AppCompatActivity;
 
 public class VoiceIdentifyActivity extends AppCompatActivity {
 
+    private EventManager eventManager;
+
+    private EventListener eventListener;
+
+    private IRecogListener iRecogListener = new IRecogListener() {
+        @Override
+        public void onAsrReady() {
+
+
+        }
+
+        @Override
+        public void onAsrBegin() {
+
+        }
+
+        @Override
+        public void onAsrEnd() {
+
+        }
+
+        @Override
+        public void onAsrPartialResult(String[] results, RecogResult recogResult) {
+
+        }
+
+        @Override
+        public void onAsrFinalResult(String[] results, RecogResult recogResult) {
+
+        }
+
+        @Override
+        public void onAsrFinish(RecogResult recogResult) {
+
+        }
+
+        @Override
+        public void onAsrFinishError(int errorCode, int subErrorCode, String errorMessage, String descMessage, RecogResult recogResult) {
+
+        }
+
+        @Override
+        public void onAsrLongFinish() {
+
+        }
+
+        @Override
+        public void onAsrVolume(int volumePercent, int volume) {
+
+        }
+
+        @Override
+        public void onAsrAudio(byte[] data, int offset, int length) {
+
+        }
+
+        @Override
+        public void onAsrExit() {
+
+        }
+
+        @Override
+        public void onAsrOnlineNluResult(String nluResult) {
+
+        }
+
+        @Override
+        public void onOfflineLoaded() {
+
+        }
+
+        @Override
+        public void onOfflineUnLoaded() {
+
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        release();
+        super.onDestroy();
+    }
+
+    private void initEventManager() {
+        eventManager = EventManagerFactory.create(this, "asr");
+        eventListener = new RecogEventAdapter(iRecogListener);
+        eventManager.registerListener(eventListener);
+
+    }
+
+    private void start(Map<String, Object> params) {
+
+        String json = new JSONObject(params).toString();
+        System.out.println(json);
+        eventManager.send(SpeechConstant.ASR_START, json, null, 0, 0);
+    }
+
+    private void stop() {
+        eventManager.send(SpeechConstant.ASR_STOP, "{}", null, 0, 0);
+
+    }
+
+    private void cancel() {
+
+        if (eventManager != null) {
+            eventManager.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+        }
+    }
+
+    public void release() {
+
+        if (eventManager == null) {
+            return;
+        }
+
+        cancel();
+        eventManager.unregisterListener(eventListener);
+        eventManager = null;
     }
 }
