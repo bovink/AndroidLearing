445c6e947a54d736c92c7f4ac9180088cd3a14c1
bovink@sina.com <261512335wen>
2018-03-05 11:34:50 +0800

    测试别名功能。

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bovink.androidlearing;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Person person = new Person(null, "23");
+        Person person = new Person("jhon", "23");
+        View view = new View(Color.parseColor("#66ccff"));
+        binding.setColorview(view);
         binding.setPerson(person);
 
     }
---
+++ app/src/main/java/com/bovink/androidlearing/View.java
@@ -0,0 +1,19 @@
+package com.bovink.androidlearing;
+
+/**
+ * @author fox
+ * @since 2018/03/05
+ */
+
+public class View {
+
+    private int color;
+
+    public View(int color) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
