5a96c3d92739edca5469f2a3ef81dcfe79049cb4
bovink <bovink@sina.com>
2018-03-14 21:50:03 +0800

    update viewstub databinding.

--- app/src/main/java/com/bovink/androidlearing/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearing/MainActivity.java
@@ -1,21 +1,49 @@
 package com.bovink.androidlearing;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
 
 import com.bovink.androidlearing.databinding.MainActBinding;
+import com.bovink.androidlearing.databinding.ViewStubBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    MainActBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        MainActBinding binding = MainActBinding.inflate(getLayoutInflater());
+        binding = MainActBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        final Person person = new Person("li", "23", 1, 2);
+        binding.viewStub.setOnInflateListener(new ViewStub.OnInflateListener() {
+            @Override
+            public void onInflate(ViewStub stub, View inflated) {
+                ViewStubBinding viewStubBinding = DataBindingUtil.bind(inflated);
+                viewStubBinding.setPerson(person);
 
+
+            }
+        });
+
+        EventHandler handler = new EventHandler();
+        binding.setHandler(handler);
+    }
+
+    public class EventHandler {
+
+        public void showView() {
+
+            if (!binding.viewStub.isInflated()) {
+                binding.viewStub.getViewStub().inflate();
+            }
+
+        }
     }
 
 
